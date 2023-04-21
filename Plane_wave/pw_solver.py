5075d63a20876d67f3a6562387cbccc293e8db58
Kristian David Torres Bautista <torresba@ualberta.ca>
2023-04-20 20:18:58 -0600

    time trimmer function and elastic ipynb

--- Plane_wave/pw_solver.py
+++ Plane_wave/pw_solver.py
@@ -1,20 +1,30 @@
 import numpy as np
 
 from examples.seismic.acoustic.wavesolver import AcousticWaveSolver
+from examples.seismic.acoustic.wavesolver import AcousticWaveSolver
 
 class PlaneWaveSolver(AcousticWaveSolver):
-    def __init__(self, *args, p=[0.], taper=True,  **kwargs):
+    def __init__(self, *args, p=[0.], taper=True, trim_t=False, **kwargs): #TODO wavelet='ricker',
         super().__init__(*args, **kwargs)
         self.rayp = p # ray parameter in s/km. Can be a list or an integer
         self.taper = taper
+        self.trim_t = trim_t
 
-    def forward(self, *args, p=None, **kwargs):#exp=0.5
+    def forward_all(self, *args, p=None, **kwargs):#exp=0.5
+        """Forward modelling of plane-waves for an array of p values.
+
+        Args:
+            p (list/1d array, optional): p values . Defaults to None.
+
+        Returns:
+            list/3d array: plane-wave gathers with dimension [Np, Nt, Nrec]
+        """
         rayp = p or self.rayp
         shots = []
         # Loop over ray-parameter
         for p in rayp: #TODO more pythonic
             # Cases for positive and negative ray parameter
-            if p>=0.:
+            if p >= 0.:
                 x0 = self.geometry.src_positions[0,0]
             else:
                 x0 = self.geometry.src_positions[-1,0]
@@ -24,11 +34,17 @@ class PlaneWaveSolver(AcousticWaveSolver):
             amplitude = self.cosine_taper(exp=0.5)
             for i in range(self.geometry.nsrc):
                 delta_t = p*(self.geometry.src_positions[i,0] - x0)
-                src.data[:, i] = self.ricker_wavelet(self.geometry.time_axis.time_values, self.geometry.f0, 1/self.geometry.f0 + delta_t, a=amplitude[i]) 
-
+                src.data[:, i] = ricker_wavelet(self.geometry.time_axis.time_values, self.geometry.f0, 1/self.geometry.f0 + delta_t, a=amplitude[i]) 
+            
             # Forward modeling and save shot gathers
-            d = super().forward(*args, src=src, **kwargs)[0]
-            shots.append(d.data)
+            d, _, _ = super().forward(*args, src=src, **kwargs)
+
+            if self.trim_t:
+                shot, _, _ = trim_time(d.data, self.geometry, self.model, p, x0)            
+            else:
+                shot = d.data
+
+            shots.append(shot)
 
         return shots 
     
@@ -44,7 +60,27 @@ class PlaneWaveSolver(AcousticWaveSolver):
         
         return amplitude 
 
-    @staticmethod
-    def ricker_wavelet(time_values, f0, t0, a=1):
-        r = (np.pi * f0 * (time_values - t0))
-        return a * (1-2.*r**2)*np.exp(-r**2)
+
+def ricker_wavelet(time_values, f0, t0, a=1):
+    r = (np.pi * f0 * (time_values - t0))
+    return a * (1-2.*r**2)*np.exp(-r**2)
+
+
+def find_nearest(time_axis, value):
+    idx = (np.abs(time_axis-value)).argmin()
+    return time_axis[idx], idx
+
+
+def trim_time(shot, geometry, model, p, x0):
+    if p >= 0.:
+        init_src_coord = np.where(geometry.src_positions==0.)[0]
+    else:
+        init_src_coord = np.where(geometry.src_positions==model.domain_size[0])[0]
+    
+    init_delta_t = p*(geometry.src_positions[init_src_coord,0] - x0) - 1./geometry.f0
+
+    init_t, idx = find_nearest(geometry.time_axis.time_values, init_delta_t)
+
+    trim_shot = shot[idx:, :]
+
+    return trim_shot, init_t, idx
